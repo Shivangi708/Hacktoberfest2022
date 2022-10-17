f3b500b2b70f02074023e8e17537269b7cae404a
LakshGujarati <100017427+LakshGujarati@users.noreply.github.com>
2022-10-17 19:21:25 +0530

    Sudokusolver

--- script.js
+++ script.js
@@ -1,63 +1,169 @@
-const addBtn = document.getElementById('add')
+var EASY_PUZZLE = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--";
+var MEDIUM_PUZZLE = "-3-5--8-45-42---1---8--9---79-8-61-3-----54---5------78-----7-2---7-46--61-3--5--";
+var HARD_PUZZLE = "8----------36------7--9-2---5---7-------457-----1---3---1----68--85---1--9----4--";
 
-const notes = JSON.parse(localStorage.getItem('notes'))
+// Set this variable to true to publicly expose otherwise private functions inside of SudokuSolver
+var TESTABLE = true;
 
-if(notes) {
-    notes.forEach(note => addNewNote(note))
-}
+var SudokuSolver = function (testable) {
+  var solver;
 
-addBtn.addEventListener('click', () => addNewNote())
+  // PUBLIC FUNCTIONS
+  function solve(boardString) {
+    var boardArray = boardString.split("");
+    if (boardIsInvalid(boardArray)) {
+      return false;
+    }
+    return recursiveSolve(boardString);
+  }
 
-function addNewNote(text = '') {
-    const note = document.createElement('div')
-    note.classList.add('note')
+  function solveAndPrint(boardString) {
+    var solvedBoard = solve(boardString);
+    console.log(toString(solvedBoard.split("")));
+    return solvedBoard;
+  }
 
-    note.innerHTML = `
-    <div class="tools">
-        <button class="edit"><i class="fas fa-edit"></i></button>
-        <button class="delete"><i class="fas fa-trash-alt"></i></button>
-    </div>
+  // PRIVATE FUNCTIONS
+  function recursiveSolve(boardString) {
+    var boardArray = boardString.split("");
+    if (boardIsSolved(boardArray)) {
+      return boardArray.join("");
+    }
+    var cellPossibilities = getNextCellAndPossibilities(boardArray);
+    var nextUnsolvedCellIndex = cellPossibilities.index;
+    var possibilities = cellPossibilities.choices;
+    for (var i = 0; i < possibilities.length; i++) {
+      boardArray[nextUnsolvedCellIndex] = possibilities[i];
+      var solvedBoard = recursiveSolve(boardArray.join(""));
+      if (solvedBoard) {
+        return solvedBoard;
+      }
+    }
+    return false;
+  }
 
-    <div class="main ${text ? "" : "hidden"}"></div>
-    <textarea class="${text ? "hidden" : ""}"></textarea>
-    `
+  function boardIsInvalid(boardArray) {
+    return !boardIsValid(boardArray);
+  }
 
-    const editBtn = note.querySelector('.edit')
-    const deleteBtn = note.querySelector('.delete')
-    const main = note.querySelector('.main')
-    const textArea = note.querySelector('textarea')
+  function boardIsValid(boardArray) {
+    return allRowsValid(boardArray) && allColumnsValid(boardArray) && allBoxesValid(boardArray);
+  }
 
-    textArea.value = text
-    main.innerHTML = marked(text)
+  function boardIsSolved(boardArray) {
+    for (var i = 0; i < boardArray.length; i++) {
+      if (boardArray[i] === "-") {
+        return false;
+      }
+    }
+    return true;
+  }
 
-    deleteBtn.addEventListener('click', () => {
-        note.remove()
+  function getNextCellAndPossibilities(boardArray) {
+    for (var i = 0; i < boardArray.length; i++) {
+      if (boardArray[i] === "-") {
+        var existingValues = getAllIntersections(boardArray, i);
+        var choices = ["1", "2", "3", "4", "5", "6", "7", "8", "9"].filter(function (num) {
+          return existingValues.indexOf(num) < 0;
+        });
+        return { index: i, choices: choices };
+      }
+    }
+  }
 
-        updateLS()
-    })
+  function getAllIntersections(boardArray, i) {
+    return getRow(boardArray, i).concat(getColumn(boardArray, i)).concat(getBox(boardArray, i));
+  }
 
-    editBtn.addEventListener('click', () => {
-        main.classList.toggle('hidden')
-        textArea.classList.toggle('hidden')
-    })
+  function allRowsValid(boardArray) {
+    return [0, 9, 18, 27, 36, 45, 54, 63, 72].map(function (i) {
+      return getRow(boardArray, i);
+    }).reduce(function (validity, row) {
+      return collectionIsValid(row) && validity;
+    }, true);
+  }
 
-    textArea.addEventListener('input', (e) => {
-        const { value } = e.target
+  function getRow(boardArray, i) {
+    var startingEl = Math.floor(i / 9) * 9;
+    return boardArray.slice(startingEl, startingEl + 9);
+  }
 
-        main.innerHTML = marked(value)
+  function allColumnsValid(boardArray) {
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8].map(function (i) {
+      return getColumn(boardArray, i);
+    }).reduce(function (validity, row) {
+      return collectionIsValid(row) && validity;
+    }, true);
+  }
 
-        updateLS()
-    })
+  function getColumn(boardArray, i) {
+    var startingEl = Math.floor(i % 9);
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8].map(function (num) {
+      return boardArray[startingEl + num * 9];
+    });
+  }
 
-    document.body.appendChild(note)
-}
+  function allBoxesValid(boardArray) {
+    return [0, 3, 6, 27, 30, 33, 54, 57, 60].map(function (i) {
+      return getBox(boardArray, i);
+    }).reduce(function (validity, row) {
+      return collectionIsValid(row) && validity;
+    }, true);
+  }
 
-function updateLS() {
-    const notesText = document.querySelectorAll('textarea')
+  function getBox(boardArray, i) {
+    var boxCol = Math.floor(i / 3) % 3;
+    var boxRow = Math.floor(i / 27);
+    var startingIndex = boxCol * 3 + boxRow * 27;
+    return [0, 1, 2, 9, 10, 11, 18, 19, 20].map(function (num) {
+      return boardArray[startingIndex + num];
+    });
+  }
 
-    const notes = []
+  function collectionIsValid(collection) {
+    var numCounts = {};
+    for(var i = 0; i < collection.length; i++) {
+      if (collection[i] != "-") {
+        if (numCounts[collection[i]] === undefined) {
+          numCounts[collection[i]] = 1;
+        } else {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 
-    notesText.forEach(note => notes.push(note.value))
+  function toString(boardArray) {
+    return [0, 9, 18, 27, 36, 45, 54, 63, 72].map(function (i) {
+      return getRow(boardArray, i).join(" ");
+    }).join("\n");
+  }
 
-    localStorage.setItem('notes', JSON.stringify(notes))
-}
+  if (testable) {
+    // These methods will be exposed publicly when testing is on.
+    solver = { 
+      solve: solve,
+      solveAndPrint: solveAndPrint,
+      recursiveSolve: recursiveSolve,
+      boardIsInvalid: boardIsInvalid,
+      boardIsValid: boardIsValid,
+      boardIsSolved: boardIsSolved,
+      getNextCellAndPossibilities: getNextCellAndPossibilities,
+      getAllIntersections: getAllIntersections,
+      allRowsValid: allRowsValid,
+      getRow: getRow,
+      allColumnsValid: allColumnsValid,
+      getColumn: getColumn,
+      allBoxesValid: allBoxesValid,
+      getBox: getBox,
+      collectionIsValid: collectionIsValid,
+      toString: toString };
+  } else {
+    // These will be the only public methods when testing is off.
+    solver = { solve: solve,
+      solveAndPrint: solveAndPrint };
+  }
+
+  return solver;
+}(TESTABLE);
