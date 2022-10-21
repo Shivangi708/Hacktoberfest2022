6234a1117ab7bef2c48ceb38e631ae5ba51363dd
Priya Jain <113535427+Priya1511jain@users.noreply.github.com>
2022-10-21 21:32:59 +0530

    Create changethebackgroundofpomodoroapp.py
    code to change the background of pomodoro app

---
+++ changethebackgroundofpomodoroapp.py
@@ -0,0 +1,19 @@
+function updateClock() {
+  const { remainingTime } = timer;
+  const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+  const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+
+  const min = document.getElementById('js-minutes');
+  const sec = document.getElementById('js-seconds');
+  const time = `${minutes}:${seconds}`;
+  min.textContent = minutes;
+  sec.textContent = seconds;
+
+  const text = timer.mode === 'pomodoro' ? 'Get back to work!' : 'Take a break!';
+  document.title = `${minutes}:${seconds} — ${text}`;
+
+  const progress = document.getElementById('js-progress');
+  progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
+}
+copy
+Changing the title of the page is as simple
